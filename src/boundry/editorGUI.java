a14b3d75875fdd7d2043a749620e2011a637cd47
Zachi Meyer <za5874@gmail.com>
2017-01-09 22:30:56 +0200

    Edited EditorGui

--- src/boundry/editorGUI.java
+++ src/boundry/editorGUI.java
@@ -2,12 +2,44 @@ package boundry;
 import java.awt.Color;
 import java.awt.Font;
 
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+
+import controller.reviewController;
+
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class editorGUI extends mainPanel {
 	public editorGUI( String name , String role) {
 		super(name,role);
 		
+		setForeground(Color.WHITE);
+		JLabel lblWelcomToIbook = new JLabel("Welcome To IBook");
+		lblWelcomToIbook.setFont(new Font("Sitka Subheading", Font.BOLD, 40));
+		lblWelcomToIbook.setHorizontalAlignment(SwingConstants.CENTER);
+		lblWelcomToIbook.setForeground(Color.WHITE);
+		lblWelcomToIbook.setBackground(Color.BLACK);
+		lblWelcomToIbook.setBounds(201, 63, 491, 67);
+		add(lblWelcomToIbook);
+		
+		
+		JLabel imgR;
+		imgR = new JLabel(new ImageIcon(editorGUI.class.getResource("/boundry/mailbox.jpg")));
+		imgR.setBounds(329, 169, 247, 137);
+		add(imgR);
+		
+		JButton btnNewButton = new JButton("Mailbox");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				reviewController.checkReview();
+			}
+		});
+		btnNewButton.setBounds(413, 371, 89, 23);
+		add(btnNewButton);
+	
 	}
 
 	public void checkReview() {
@@ -39,5 +71,4 @@ public class editorGUI extends mainPanel {
 		// TODO - implement editorGUI.chooseReview
 		throw new UnsupportedOperationException();
 	}
-
 }
